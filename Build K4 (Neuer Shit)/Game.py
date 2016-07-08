b71cf6854df8c74920dc8a00879e7529c94b4dfc
Chaoste <t-kellermeier@hotmail.de>
2016-07-09 00:09:01 +0200

    Monte carlo tree search

--- Build K4 (Neuer Shit)/Game.py
+++ Build K4 (Neuer Shit)/Game.py
@@ -5,151 +5,163 @@ from EventManager import EventManager
 from MachineGUI import MachineGUI
 from MainKI import MainKI
 from PatternKI import *
+from mcts import MonteCarloTreeSearch
+from copy import deepcopy, copy
+
 
 class Game:
-    
-    
-    
+
+
+
     def __init__(self, m, n, mode):
-        
-        # init all events
-        EventManager.initEvents()
-        
-        EventManager.subscribe("GameFinished", self.onGameFinished)
-        EventManager.subscribe("GameStarted", self.onGameStarted)
-        
+        # move counter init
+        self.moveCounter = 0
+
         # save size and mode
         self.size = [m,n]
         self.mode = mode
-        
+
         # just to init the value
         self._currentPlayer = 1
         self._currentPlayerType = "human"
-        
+
+        if mode == "copy":
+            return
+
+        # init all events
+        EventManager.initEvents()
+
+        EventManager.subscribe("GameFinished", self.onGameFinished)
+        EventManager.subscribe("GameStarted", self.onGameStarted)
+
         # instantiate model and view
         self.HexBoard = HexBoard(self.size[0], self.size[1])
         self.HexBoard.setReferenceToGame(self)
-        
+
         if self.UIRequired():
             self.HexGUI = HexGUI(self.size[0], self.size[1], self)
         else:
             self.MachineGUI = MachineGUI(self.size[0], self.size[1], self)
-        
+
         if self.mode == "ki" or self.mode == "machine":
-            self.KI = PatternKI(self)
-        
+            self.KI = MonteCarloTreeSearch(self, 2)
+
         # set the game to entry point
         self.start(self._currentPlayer)
-        
+
         if self.UIRequired():
             # main loop starts for event receiving
             self.HexGUI.mainloop()
-        
+
         if self.UIRequired() == False:
             self.MachineGUI.gameLoop()
-            
+
     def UIRequired(self):
         if self.mode == "human" or self.mode == "ki" or self.mode == "inter":
             return True
         else:
             return False
-    
+
     def start(self, firstPlayer):
-        
+
         EventManager.notify("GameStarting")
-        
+
         # move counter init
         self.moveCounter = 0
-        
+
         # generate fresh state
         self.HexBoard = HexBoard(self.size[0], self.size[1])
         self.HexBoard.setReferenceToGame(self)
-        
+
         # current player depending on decision
         self._currentPlayer = firstPlayer
-        
+
         if self.mode == "ki" or self.mode == "machine":
-            self.KI = PatternKI(self)
-        
+            self.KI = MonteCarloTreeSearch(self, 2)
+
         # if random number wanted, generate one
         if firstPlayer == 0:
             self.chooseFirst()
-        
+
         EventManager.notify("GameStarted")
-    
+
     def onGameStarted(self):
-        
+
         if self.UIRequired():
             # draw the gui
             self.HexGUI.draw()
-        
+
     # Game finished event
     def onGameFinished(self):
-        
+
         if self.UIRequired():
             # move over to main menu and present the winner
             self.HexGUI.openPage("menu")
             self.HexGUI.won(self.HexBoard.winner())
-    
+
     # generate random player number
     def chooseFirst(self):
         self._currentPlayer = round(random.random()) + 1
-    
+
     # is the getter for the private variable
     def currentPlayer(self):
         return self._currentPlayer
-    
+
     # is the current Player human?
     def isPlayerHuman(self):
         return self._currentPlayerType == "human"
-    
+
     # alter the players
     def changePlayer(self):
-        
+
         if self._currentPlayer == 1:
             self._currentPlayer = 2
-            
+
         else:
             self._currentPlayer = 1
-            
+
         if self.mode == "ki":
-            
+
             if self._currentPlayerType == "human":
                 self._currentPlayerType = "ki"
             else:
                 self._currentPlayerType = "human"
-                
-    
+
+
     # control flow for click event
     def makeMove(self, move):
-        
-        # Notify 
+
+        # Notify
         EventManager.notify("MoveBegan")
-        
+
         # if already marked dont do anything
         if self.HexBoard.isMarked(move[1], move[0]):
             EventManager.notify("MoveDenied")
-            
+
         else:
-            
+
             # otherwise count the click
             self.moveCounter = self.moveCounter + 1
-            
+
             # notify Model
             self.HexBoard.receiveMove(move)
-            
+
             # notify View
             self.changePlayer()
             EventManager.notify("PlayerChanged")
-            
-            if not self.isPlayerHuman():
+
+            if not self.isPlayerHuman() and self.mode != "copy":
                 move = self.KI.getMove()
                 self.makeMove(move)
-            
+
         EventManager.notify("MoveFinished")
-        
-        
-        
-        
-        
-        
+
+    def copy(self):
+        copy_obj = type(self)(*self.size, mode="copy")
+        copy_obj.HexBoard = copy(self.HexBoard)
+        copy_obj.HexBoard.Vertices = deepcopy(self.HexBoard.Vertices)
+        copy_obj.HexBoard.Game = copy_obj
+        copy_obj._currentPlayer = self._currentPlayer
+        copy_obj._currentPlayerType = self._currentPlayerType
+        copy_obj.moveCounter = self.moveCounter
+        return copy_obj
--- Build K4 (Neuer Shit)/mcts.py
+++ Build K4 (Neuer Shit)/mcts.py
@@ -1,5 +1,7 @@
 import math
-
+import random
+from KI import KI
+from EventManager import EventManager
 
 C = math.sqrt(2)
 INF = float('inf')
@@ -7,12 +9,17 @@ N = 1000
 
 class Node:
 
-    def __init__(self, parent, mcts, value=0):
+    def __init__(self, parent, game, value=0):
         self.parent = parent
-        self.mcts = mcts
         self._children = None
         self.value = value
         self.simulations = 0
+        self.game = game
+
+    @property
+    def is_ki_turn(self):
+        # FIXME mcts missing
+        self.is_ki_turn = self.game.currentPlayer() == self.mcts.player_id
 
     def add_children(*child_nodes):
         self._children += child_nodes
@@ -20,15 +27,26 @@ class Node:
     @property
     def children(self):
         if self._children is None:
-            self.mcts.expand(self)
+            self._children = list(self.expand(self))
         return self._children
+
+    def expand(self, node):
+        # Get all legal actions for this node. Create node object for
+        # each action and add it to the tree / childrens of the given node
+        for i in range(node.game.size[0]):
+            for j in range(node.game.size[1]):
+                vertex = node.game.HexBoard.getVertex(i,j)
+                if vertex.player == None:
+                    future_game = node.game.copy()
+                    future_game.makeMove([j, i])  # TODO: Right order?
+                    yield Node(node, future_game)
 
     def backpropagation_value(self):
         # pass mean, max, robust max or mix -> for now try mean
         return self.value / self.simulations
 
     def backpropagate(self, value=None):
-        if None:
+        if value is None:
             self.parent.backpropagate(self.backpropagation_value())
             return
         self.simulations += 1
@@ -41,41 +59,75 @@ class Node:
             return INF
         avg_value = self.value / self.simulations
         confidence = C * math.sqrt(
-            math.log(self.parent.simulations) / self.simulations))
+            math.log(self.parent.simulations) / self.simulations)
         return avg_value + confidence
 
 
-class MonteCarloTreeSearch:
+class MonteCarloTreeSearch(KI):
 
-    def __init__(self):
+    def __init__(self, game, player_id):
+        super().__init__(game)
         # Initialize tree with a root node (which parent is None)
-        self.root = Node(None, self)
-        self.initial_simulation()
+        self.player_id = 2  # TODO: Pass the number of the ki player
+        self.root = Node(None, game)
+        self.moves = []
+        for child in self.root.children:
+            self.simulate(child, N=5)
+        if len(self.moves) == 0:
+            self.explore_tree()
+        print("First level Initialized")
 
-    def __call__(self):
+    def explore_tree(self):
+        node = self.select_node()
+        # node becomes None if the game is finished at this point
+        while node is not None:
+            self.simulate(node)
+            node.backpropagate()
+            print("Explored node: {} simulations, {} avg value".format(node.simulations, node.value / node.simulations))
+            node = self.select_node()
+
+    def getMove(self):
         # Called by framework to return the next step
-        new_node = self.select_node()
-        self.simulate(new_node)
-        new_node.backpropagate()
         return None
 
     def select_node(self):
         # UCT process to decide if a node is expanded or a new node is explored
         node, uct_value = self.root, self.root.uct()
-        # explore the tree until an uncalculated node is found
-        while uct_value < INF:
-            children = [(child, child.uct() for child in node.children]
+        # Explore the tree until an uncalculated node is found
+        while uct_value is not INF:
+            children = [(child, child.uct()) for child in node.children]
+            if any([x.game.HexBoard.finished() for x in children]):
+                return None
+            # Expand nodes with a high UCT rank
             children.sort(key=lambda x: x[1], reverse=True)
             node, uct_value = children[0]
         return node
 
-    def expand(self, node):
-        # Get all legal actions for this node. Create node object for
-        # each action and add it to the tree / childrens of the given node
-        # TODO
-        pass
+    @staticmethod
+    def options(game):
+        for i in range(game.size[0]):
+            for j in range(game.size[1]):
+                vertex = game.HexBoard.getVertex(i,j)
+                if vertex.player == None:
+                    yield [j, i]
 
-    def simulate(self, node):
-        # Do N simulations and save their results
-        # TODO
-        pass
+    def simulate(self, node, N=50):
+        success_sum = 0
+        for _ in range(N):
+            callbacks = EventManager.Events["GameFinished"][:]
+            callbacks2 = EventManager.Events["MoveFinished"][:]
+            EventManager.Events["GameFinished"].clear()
+            EventManager.Events["MoveFinished"].clear()
+            simulation = node.game.copy()
+            EventManager.subscribe("GameFinished", simulation.HexBoard.onGameFinished)
+            while not simulation.HexBoard.finished():
+                options = list(self.options(simulation))
+                assert len(options) > 0, "No options left but not finished"
+                move = options[round(random.random() * (len(options) - 1))]
+                simulation.makeMove(move)
+            EventManager.Events["GameFinished"] = callbacks
+            EventManager.Events["MoveFinished"] = callbacks2
+            if simulation.HexBoard.winner() == self.player_id:
+                success_sum += 1
+        node.simulations += N
+        node.value += success_sum
